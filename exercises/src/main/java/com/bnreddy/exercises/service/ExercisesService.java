2a0ce4f6bf352c819137b234aab8eabe5a8673a7
Niranjan Reddy Boddireddy <niranjanreddyboddireddy@Niranjan-Reddy-Boddireddy.local>
2025-09-13 21:44:44 +0530

    addition of two string completed

--- exercises/src/main/java/com/bnreddy/exercises/service/ExercisesService.java
+++ exercises/src/main/java/com/bnreddy/exercises/service/ExercisesService.java
@@ -6,6 +6,33 @@ import org.springframework.stereotype.Service;
 public class ExercisesService {
 
     public String add(String firstString, String secondString) {
-        return String.valueOf(Long.parseLong(firstString) + Long.parseLong(secondString));
+
+        firstString = new StringBuilder(firstString).reverse().toString();
+        secondString = new StringBuilder(secondString).reverse().toString();
+
+        int maxLen = Math.max(firstString.length(), secondString.length());
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < maxLen; i++) {
+            char firstCh = firstString.length() > i ? firstString.charAt(i) : '0';
+            char secondCh = secondString.length() > i ? secondString.charAt(i) : '0';
+
+            int sum = Integer.parseInt(String.valueOf(firstCh)) + Integer.parseInt(String.valueOf(secondCh)) + carry;
+            carry = 0;
+
+            if (sum > 9) {
+                carry = 1;
+                sum = sum % 10;
+            }
+
+            sb.append(sum);
+        }
+
+        if (carry > 0) {
+            sb.append(carry);
+        }
+
+        return sb.reverse().toString();
     }
 }
